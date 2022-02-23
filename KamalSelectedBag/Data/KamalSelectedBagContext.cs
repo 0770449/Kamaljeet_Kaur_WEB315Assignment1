39a3db2e01e57a08480ab08e81a18514bc1892cc
Kamaljeet Kaur <jeetkamalkaur303@gmail.com>
2022-02-23 18:06:31 -0500

    kk

---
+++ KamalSelectedBag/Data/KamalSelectedBagContext.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using KamalSelectedBag.Models;
+
+namespace KamalSelectedBag.Data 
+{
+public class KamalSelectedBagContext : DbContext
+    {
+        public KamalSelectedBagContext (DbContextOptions<KamalSelectedBagContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<KamalSelectedBag.Models.Bag> Bag { get; set; }
+    }
+}   
